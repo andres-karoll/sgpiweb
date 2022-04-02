1f126a726404d91c7742d98f1da8415b782e1f71
Sam Cuervo <50210991+SamCuervo@users.noreply.github.com>
2022-04-01 22:02:30 -0500

    lider semilleero

--- src/pages/Proyectos/Presupuesto/PresupuestoProyecto.js
+++ src/pages/Proyectos/Presupuesto/PresupuestoProyecto.js
@@ -101,7 +101,7 @@ status: false,
 
     </ul>
     {
-             rol==="Docente investigador" ?(
+             rol==="Docente investigador"||rol==="Docente lider semillero" ?(
              
               <></>
              
@@ -110,7 +110,7 @@ status: false,
              )
            }
  {
-             rol==="Docente investigador" ?(
+             rol==="Docente investigador"|| rol==="Docente lider semillero" ?(
              
               <></>
              
